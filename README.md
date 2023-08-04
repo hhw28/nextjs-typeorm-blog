9a902eae4ccb2862631498efb5e52e1ebb8f1fbe
hhw <196912334@qq.com>
2023-08-04 15:45:37 +0800

    init 数据库

--- README.md
+++ README.md
@@ -1,17 +1,56 @@
-# 初始代码
+## 清空之前的开发环境
+
+```
+docker ps
+docker kill 容器id
+docker rm 容器id
+
+rm -rf blog-data
+或
+docker container prune 
+docker volume rm blog-data
+```
+
+## 启动数据库
+
+```
+docker run -v "$PWD/blog-data":/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_USER=blog -e POSTGRES_HOST_AUTH_METHOD=trust -d postgres:12.2
+```
+
+## 创建数据库
+
+```
+// 进入docker
+docker exec -it <id> bash
+
+// 使用POSTGRES_USER连接postgres
+psql -U blog
+
+// 创建名为blog_development的数据库
+CREATE DATABASE blog_development ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8';
+```
+
+## 修改数据表
+
+1、修改`ormconfig.json`中的`host`，mac为localhost，windows需要更改为本机ip地址，端口号为启动docker时使用的端口号
+
+2、`yarn m:run`生成数据表
+
+3、`node dist/seed.js`填充数据
 
 ## 开发
 
-```bash
+```
 yarn dev
-# or
-npm run dev
+
+// 做了两件事
+// 启用本地服务
+// 指定编译src中的ts => 产出js到dist
 ```
 
 ## 部署
 
-```bash 
+```
 yarn build
 yarn start
 ```
-
--- dist/seed.js
+++ dist/seed.js
@@ -10,16 +10,49 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _User = require("./entity/User");
+
+var _Post = require("./entity/Post");
+
+var _Comment = require("./entity/Comment");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var manager, u1, p1, c1;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log(connection);
-            connection.close();
+            manager = connection.manager; // 创建 user 1
 
-          case 2:
+            u1 = new _User.User();
+            u1.username = 'hannah';
+            u1.passwordDigest = 'xxx';
+            _context.next = 6;
+            return manager.save(u1);
+
+          case 6:
+            // 创建 post 1
+            p1 = new _Post.Post();
+            p1.title = 'Post 1';
+            p1.content = 'My First Post';
+            p1.author = u1;
+            _context.next = 12;
+            return manager.save(p1);
+
+          case 12:
+            c1 = new _Comment.Comment();
+            c1.user = u1;
+            c1.post = p1;
+            c1.content = 'Awesome!';
+            _context.next = 18;
+            return manager.save(c1);
+
+          case 18:
+            connection.close();
+            console.log('OK!');
+
+          case 20:
           case "end":
             return _context.stop();
         }

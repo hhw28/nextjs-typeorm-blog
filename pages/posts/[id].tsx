2ece2d8f151477df2366774b6637c61662ffca42
hhw <196912334@qq.com>
2023-08-21 15:07:48 +0800

    完善当前页面css

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,40 +2,81 @@ import React from 'react';
 import { GetServerSideProps, NextPage } from 'next';
 import { getDatabaseConnection } from 'lib/getDatabaseConnection';
 import { Post } from 'src/entity/Post';
-import Link  from 'next/link';
+import Link from 'next/link';
+import { withSession } from 'lib/withSession';
+import { marked } from 'marked';
 
 type Props = {
   post: Post;
   id: number;
+  currentUser: string;
 };
 const postsShow: NextPage<Props> = (props) => {
-  const { post } = props;
+  const { post, currentUser } = props;
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <article dangerouslySetInnerHTML={{ __html: post.content }}></article>
-      <Link href={`/posts/edit?id=${props.id}`}>
-        <button>修改</button>
-      </Link>
-      <Link href={`/posts`}>
-        <button>返回列表</button>
-      </Link>
-    </div>
+    <>
+      <div className="wrapper">
+        <header>
+          <h1>{post.title}</h1>
+          {currentUser && (
+            <p className="actions">
+              <Link href={`/posts/edit?id=${props.id}`}>
+                {/* <Link href="/posts/[id]/edit" as={`/posts/${post.id}/edit`}> */}
+                <button>编辑</button>
+              </Link>
+              <Link href={`/posts`}>
+                <button>返回列表</button>
+              </Link>
+            </p>
+          )}
+        </header>
+        <article
+          className="markdown-body"
+          // dangerouslySetInnerHTML={{ __html: marked(post.content) }}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        ></article>
+      </div>
+      <style jsx>{`
+        .actions > * {
+          margin: 4px;
+        }
+        .actions > *:first-child {
+          margin-left: 0;
+        }
+        .wrapper {
+          max-width: 800px;
+          margin: 16px auto;
+          padding: 0 16px;
+        }
+        h1 {
+          padding-bottom: 16px;
+          border-bottom: 1px solid #666;
+        }
+      `}</style>
+    </>
   );
 };
 
 export default postsShow;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connection = await getDatabaseConnection();
+export const getServerSideProps: GetServerSideProps = withSession(
   //@ts-ignore
-  const post = await connection.manager.findOne(Post, context.params.id);
+  async (context: GetServerSidePropsContext) => {
+    const connection = await getDatabaseConnection();
 
-  return {
-    props: {
-      post: JSON.parse(JSON.stringify(post)),
-      id: context.params.id
-    },
-  };
-};
+    const id = context.query.id;
+    //@ts-ignore
+    const post = await connection.manager.findOne(Post, id);
+
+    const currentUser = (context.req as any).session.get('currentUser') || null;
+
+    return {
+      props: {
+        post: JSON.parse(JSON.stringify(post)),
+        id,
+        currentUser: JSON.parse(JSON.stringify(currentUser || '')),
+      },
+    };
+  },
+);
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,11 @@
-import { GetServerSideProps, NextPage } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
 import { getDatabaseConnection } from 'lib/getDatabaseConnection';
 import { Post } from 'src/entity/Post';
 import Link from 'next/link';
 import { usePager } from 'hooks/usePager';
 import { useCallback } from 'react';
 import axios from 'axios';
+import { withSession } from 'lib/withSession';
 
 type Props = {
   posts: Post[];
@@ -12,63 +13,136 @@ type Props = {
   page: number;
   total: number;
   totalPage: number;
+  currentUser: string;
 };
 const index: NextPage<Props> = (props) => {
-  const { posts, page, total, totalPage } = props;
+  const { posts, page, total, totalPage, currentUser } = props;
 
   const { pager } = usePager({ page, total, totalPage });
 
   const onDelete = useCallback((id: number) => {
-    axios.delete(`/api/v1/posts`, { data: { id } }).then((res) => {
-      window.location.reload()
-    });
+    axios.delete(`/api/v1/posts`, { data: { id } }).then(
+      (res) => {
+        window.alert('删除成功');
+        window.location.reload();
+      },
+      () => {
+        window.alert('删除失败');
+      },
+    );
   }, []);
 
   return (
-    <div>
-      <h1>文章列表</h1>
-      <Link href={'/posts/new'}>
-        <button>新增</button>
-      </Link>
-      <ul>
+    <>
+      <div className="posts">
+        <header>
+          <h1>文章列表</h1>
+          {currentUser && (
+            <Link href="/posts/new">
+              <button>新增文章</button>
+            </Link>
+          )}
+          <Link href="/">
+            <button>主页</button>
+          </Link>
+        </header>
+
         {posts.map((post) => (
-          <li key={post.id}>
+          <div className="onePost" key={post.id}>
             <Link href={`/posts/${post.id}`}>
-              <a>{post.title}</a>
+              <span>{post.title}</span>
             </Link>
 
-            <button onClick={() => onDelete(post.id)}>删除</button>
-          </li>
+            <div>
+              <Link href={`/posts/${post.id}`}>
+                <button>查看</button>
+              </Link>
+              <button onClick={() => onDelete(post.id)}>
+                删除
+              </button>
+            </div>
+          </div>
         ))}
-      </ul>
-      <footer>{pager}</footer>
-    </div>
+
+        <footer className="footer">{pager}</footer>
+      </div>
+      <style jsx>{`
+        .posts {
+          max-width: 800px;
+          margin: 0 auto;
+          padding: 16px;
+        }
+        .posts > header {
+          display: flex;
+          align-items: center;
+        }
+        .posts > header > h1 {
+          margin: 0;
+          margin-right: auto;
+        }
+        .onePost {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+          border-bottom: 1px solid #ddd;
+          padding: 8px 0;
+        }
+        .onePost > span {
+          border-bottom: none;
+          color: #000;
+          cursor: pointer;
+        }
+        .onePost > span:hover {
+          color: #00adb5;
+        }
+        .footer {
+          margin-top: 16px;
+          display: flex;
+          flex-direction: row;
+          justify-content: flex-end;
+        }
+      `}</style>
+        <style jsx global>
+        {`
+          button + button {
+            margin: 6px;
+          }
+        `}
+      </style>
+    </>
   );
 };
 export default index;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { query } = context;
+export const getServerSideProps: GetServerSideProps = withSession(
+  // @ts-ignore
+  async (context: GetServerSidePropsContext) => {
+    const { query } = context;
 
-  const page = parseInt(query.page?.toString()) || 1;
-  const pageSize = 10;
+    const page = parseInt(query.page?.toString()) || 1;
+    const pageSize = 10;
 
-  const connection = await getDatabaseConnection();
-  const [posts, count] = await connection.manager.findAndCount(Post, {
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-  });
+    const connection = await getDatabaseConnection();
+    const [posts, count] = await connection.manager.findAndCount(Post, {
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+    });
 
-  const total = count;
-  const totalPage = Math.ceil(total / pageSize);
+    const total = count;
+    const totalPage = Math.ceil(total / pageSize);
 
-  return {
-    props: {
-      posts: JSON.parse(JSON.stringify(posts)),
-      pageSize,
-      page,
-      total,
-      totalPage,
-    },
-  };
-};
+    const currentUser = (context.req as any).session.get('currentUser') || null;
+
+    return {
+      props: {
+        posts: JSON.parse(JSON.stringify(posts)),
+        pageSize,
+        page,
+        total,
+        totalPage,
+        currentUser: JSON.parse(JSON.stringify(currentUser || '')),
+      },
+    };
+  },
+);

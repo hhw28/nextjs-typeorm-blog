eed0d20b34eea7070f7ccccbfac36b1f0f288b7c
hhw <196912334@qq.com>
2023-08-25 10:45:10 +0800

    update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,5 +11,6 @@ yarn m:run &&
 git reset --hard HEAD && 
 docker build -t hhw/node-web-app . &&
 docker kill app &&
+docker rm app &&
 docker run --name app --network=host -p 3000:3000 -d hhw/node-web-app &&
 echo 'ok!'
